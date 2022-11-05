0c1d129bca1843dc1dded68311d799f228b05dca
Darina Yaroshchuk <goodflo99@gmail.com>
2022-11-05 20:52:32 +0200

    miggrated from css to scss

--- TrackEdize/ClientApp/src/app/tracking/issue-view/issue-view.component.ts
+++ TrackEdize/ClientApp/src/app/tracking/issue-view/issue-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-issue-view',
   templateUrl: './issue-view.component.html',
-  styleUrls: ['./issue-view.component.css']
+  styleUrls: ['./issue-view.component.scss']
 })
 export class IssueViewComponent implements OnInit {
 
